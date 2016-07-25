624f03654019b80ede925d6c8db7db5fdb78b154
Jelco <omni88@hotmail.com>
2016-07-25 12:58:52 +0200

    Initial commit 2

---
+++ test.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<link type="text/css" rel="stylesheet" href="style.css"/>
+		<title>Remnant - The Game</title>
+	</head>
+	<body>
+		<?php
+			echo 'It\'s a PHP script!';
+		?>
+	</body>
