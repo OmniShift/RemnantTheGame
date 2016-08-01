24a1b1917674034d9e548bbddb4e3f589e6b1150
Jelco <omni88@hotmail.com>
2016-08-01 12:03:24 +0200

    query test 6

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const   fs				= require('fs'),
 				.on('row', function(row) {
 					console.log(JSON.stringify(row));
 				});
-			console.log(client.query("SELECT COUNT(*) FROM 'TakenIDs' WHERE IDname='AdmUsr';"));
+			client.query("SELECT COUNT(*) FROM 'TakenIDs' WHERE IDname='AdmUsr';");
 		});
 
 app.set('port', (process.env.PORT || 5000));
