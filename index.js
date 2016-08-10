43ccc759259c12993ccd5c25b9d08a6a99a9d2c4
Jelco <omni88@hotmail.com>
2016-08-10 22:22:23 +0200

    lobby cont 8

--- index.js
+++ index.js
@@ -132,19 +132,20 @@ io.on('connection', function(socket) {
 	socket.on('update lobby info', function(roomID, pNumber, pID, pReady, pCommName, pKingdomPref) {
 		pg.connect(process.env.DATABASE_URL, function(err, client) {
 			if (err) throw err;
-		client.query('UPDATE "GRIDs" SET playerid[' + pNumber + '] = \'' + pID + '\', playerready[' + pNumber + '] = ' + pReady + ', playercommname[' + pNumber + '] = \'' + pCommName + '\', playerkingdompref[' + pNumber + '] = ' + pKingdomPref + ' WHERE idname=\'' + roomID + '\';', function(err, data) {
-			if(err) {
-				throw new Error('Error updating room ' + roomID + ' with new info');
-			};
-		});
-		client
-		 .query('SELECT (playerid, playerready, playercommname, playerkingdompref) FROM "GRIDs" WHERE idname=\'' + roomID + '\';')
-		 .on('row', function(err, row) {
-			if(err) {
-				throw new Error('Error selecting room ' + roomID + ' player status info');
-			};
-		 	console.log(JSON.stringify(row));
-		 	socket.broadcast.to(roomID).emit('update lobby info', row);
+			client.query('UPDATE "GRIDs" SET playerid[' + pNumber + '] = \'' + pID + '\', playerready[' + pNumber + '] = ' + pReady + ', playercommname[' + pNumber + '] = \'' + pCommName + '\', playerkingdompref[' + pNumber + '] = ' + pKingdomPref + ' WHERE idname=\'' + roomID + '\';', function(err, data) {
+				if(err) {
+					throw new Error('Error updating room ' + roomID + ' with new info');
+				};
+			});
+			client
+			 .query('SELECT (playerid, playerready, playercommname, playerkingdompref) FROM "GRIDs" WHERE idname=\'' + roomID + '\';')
+			 .on('row', function(err, row) {
+				if(err) {
+					throw new Error('Error selecting room ' + roomID + ' player status info');
+				};
+			 	console.log(JSON.stringify(row));
+			 	socket.broadcast.to(roomID).emit('update lobby info', row);
+			});
 		});
 	});
 
