1ec5ed9a951436d8f45a38f6698cfb384829c6ab
Jelco <omni88@hotmail.com>
2016-09-23 14:45:49 +0200

    randomization fix

--- index.js
+++ index.js
@@ -298,14 +298,14 @@ io.on('connection', function (socket) {
             var playerKingdomOrder = [0,0,0,0];
             var tempOrderArray = [0,1,2,3];
             //randomizing player order
-            var currentIndex = tempOrderArray.length, temporaryValue, randomIndex;
+            /*var currentIndex = tempOrderArray.length, temporaryValue, randomIndex;
             while (0 !== currentIndex) {
                 randomIndex = Math.floor(Math.random() * currentIndex);
                 currentIndex -= 1;
                 temporaryValue = tempOrderArray[currentIndex];
                 tempOrderArray[currentIndex] = tempOrderArray[randomIndex];
                 tempOrderArray[randomIndex] = temporaryValue;
-            }
+            }*/
             logger.log('starting select query');
             pool.query('SELECT * FROM "GRIDs" WHERE idname = $1;', [roomID]).then(res => {
                 logger.log('query result: ' + JSON.stringify(res));
@@ -355,7 +355,6 @@ io.on('connection', function (socket) {
                 logger.log('Commander name order: ' + JSON.stringify(playerCommOrder));
                 logger.log('player per kingdom: ' + JSON.stringify(pPosPerKingdom));
                 logger.log('preferences per player: ' + JSON.stringify(playerKingdomOrder));
-                logger.log('testing');
             }).then(function() {
                 pool.query(
                     'UPDATE "GRIDs" SET status = 1, playerid = $1, playercommname = $2, playerkingdompref = $3,  WHERE idname = $4;', [
