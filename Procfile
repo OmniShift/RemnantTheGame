54ca36369a3d77c5268338ac98bbebb98610f5f9
Jelco <omni88@hotmail.com>
2017-07-13 10:56:25 +0200

    DoS vulnrability fix (and loads of local changes)

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: node index.js
+log: tail -f logs/test.log
--- index.js
+++ index.js
@@ -8,6 +8,7 @@ var app = express();
 var http = require('http');
 var server = http.createServer(app);
 var io = require('socket.io').listen(server);
+//var socket = io();
 
 app.set('port', (process.env.PORT || 5000));
 server.listen(app.get('port'), function () {
@@ -16,9 +17,23 @@ server.listen(app.get('port'), function () {
 
 app.use(express.static(__dirname + '/static'));
 
+//alternative method, untested
+app.get('/', function (request, response) {
+    response.sendFile(__dirname + '/index.html');
+    logger.log('on main page');
+});
 app.get('/', function (request, response) {
     response.render('pages/index');
+    logger.log('on main page');
 });
-app.get('/game', function (request, response) {
-    response.render('pages/game');
+app.get('/game.html', function (request, response) {
+    response.render('/game.html');
+    logger.log('on game page');
+});
+app.get('/game.html', function (request, response) {
+    response.sendFile(__dirname + '/game.html');
+    logger.log('on game page');
+});
+io.on('next turn', function (a, b) {
+    logger.log('next turn()');
 });
